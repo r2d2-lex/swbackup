05e9f72ebfdf4c6b1bcb33023f5090b70f27c906
Gogs <r2d2-lex@yandex.ru>
2024-01-30 10:59:11 +0500

    fix импорт функции в тест

--- tests/test_snmp2.py
+++ tests/test_snmp2.py
@@ -1,4 +1,6 @@
 from unittest import TestCase
+from snmp2 import check_hex_string
+
 
 """
     iso.3.6.1.2.1.1.1.0 = Hex-STRING: 41 6C 6C 69 65 64 57 61 72 65 20 50 6C 75 73 20 
@@ -6,7 +8,7 @@ from unittest import TestCase
 """
 
 
-class SnmpTestCase(TestCase):
+class CheckHexStringTestCase(TestCase):
 
     def test_snmp2_module_exists(self):
         try:
@@ -14,37 +16,32 @@ class SnmpTestCase(TestCase):
         except ModuleNotFoundError:
             self.fail('Module snmp2 not found')
 
-    def test_check_hex_string_exists(self):
+    def test_check_hex_string_function_exists(self):
         import snmp2
         self.assertTrue(
             hasattr(snmp2, 'check_hex_string')
         )
 
     def test_check_hex_string_args(self):
-        from snmp2 import check_hex_string
         try:
             check_hex_string('string')
         except TypeError:
             self.fail('Function must take one arg')
 
     def test_check_hex_string_raises_on_invalid_argument_type(self):
-        from snmp2 import check_hex_string
         with self.assertRaises(TypeError):
             check_hex_string(None)
 
     def test_check_hex_string_returns_string_type(self):
-        from snmp2 import check_hex_string
         result = check_hex_string('string')
         self.assertIsInstance(result, str)
 
     def test_check_hex_string_returns_right_value(self):
-        from snmp2 import check_hex_string
         arg = 'iso.3.6.1.2.1.1.1.0 = Hex-STRING: 41 6C 6C 69 65 64 57 61 72 65 20 50 6C 75 73 20'
         result = check_hex_string(arg)
         self.assertEqual(result, 'AlliedWare Plus')
 
     def test_check_hex_string_must_be_hex_value(self):
-        from snmp2 import check_hex_string
         arg = 'iso.3.6.1.2.1.1.1.0 = Hex-STRING: x'
         try:
             check_hex_string(arg)
@@ -52,8 +49,6 @@ class SnmpTestCase(TestCase):
             self.fail('arg must be hex value')
 
     def test_check_hex_string_must_returns_string(self):
-        pass
-        from snmp2 import check_hex_string
         arg = 'Hex-STRING: 41 6C 6C 69 65 64 57 61 72 65 20 50 6C 75 73 20 28 54 4D 29 20 32 2E 31 2E 32 CE 20'
         try:
             check_hex_string(arg)
@@ -61,6 +56,5 @@ class SnmpTestCase(TestCase):
             self.fail('must_returns_string')
 
     def test_check_hex_string_returns_right_value2(self):
-        from snmp2 import check_hex_string
         result = check_hex_string('string')
         self.assertEqual(result, 'string')
